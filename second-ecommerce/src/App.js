7d2a575751706d9ac3816f8b98a97a1fe79eaa11
Emiliano Fernandez Mora <emilianofernandezmora@hotmail.com>
2022-05-31 18:34:13 -0300

    Primer commit

---
+++ second-ecommerce/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+
+
+function App() {
+  return (
+    <>
+      
+      <NavBar />
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
